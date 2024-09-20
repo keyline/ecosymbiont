48930fbfbe38f382a18db7fd899069f87081aa53
Deblina Das <deblina@keylines.net>
2024-09-20 14:24:07 +0530

    frontend 20-09-2024 chnges

--- resources/views/front/pages/signup.blade.php
+++ resources/views/front/pages/signup.blade.php
@@ -7,6 +7,20 @@
                     <div class="block-content">
                         <!-- single-post box -->
                         <div class="row">
+                          <div class="col-xl-12">
+                              @if(session('success_message'))
+                                <div class="alert alert-success bg-success text-dark border-0 alert-dismissible show autohide" role="alert">
+                                  {{ session('success_message') }}
+                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                </div>
+                              @endif
+                              @if(session('error_message'))
+                                <div class="alert alert-danger bg-danger text-dark border-0 alert-dismissible show autohide" role="alert">
+                                  {{ session('error_message') }}
+                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                </div>
+                              @endif
+                            </div>
                             <div class="col-md-12">
                                 <div class="card">
                                     <div class="card-body">
