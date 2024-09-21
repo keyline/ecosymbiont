17f27c34ee6ed1cce342125c27e35208799085e0
surajit bera <surajit.b@keylines.net>
2024-09-22 00:05:28 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -1,70 +1,291 @@
 <?php
-use App\Models\GeneralSetting;
-use App\Models\Article;
-$generalSetting            = GeneralSetting::find('1');
-?>
+   use App\Models\GeneralSetting;
+   use App\Models\User;
+   use App\Models\Property;
+   use App\Models\Unit;
+   use App\Models\PropertyInspection;
+   use App\Models\RoomType;
+   $generalSetting               = GeneralSetting::find('1');
+   $getUser                      = User::select('first_name', 'last_name')->where('id', '=', $user_id)->first();
+   ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecosymbion</title>
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-        h1,h2,h3,h4,h5,h6,p,span,ul,li,a{
-            margin: 0;
-            padding: 0;
-        }
-        p{
-            font-size: 12px;
-        }
-        ol { counter-reset: item }
-        ol li{ display: block; line-height: 1.5; position: relative; padding-left: 38px;}
-        li:before { content: counters(item, ".") " "; counter-increment: item ; margin-right: 10px; position: absolute; top: 0; left: 0;}
-        ol > strong > li{
-            padding-left: 15px;
-        }
-        ul > li::before{
-            display: none;
-        }
-    </style>
-</head>
-<body style="background: #e7e6e6; font-family: sans-serif;">
-    <table style="width: 100%; background: #fff; margin:0 auto; padding: 30px;">
-        <tbody>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>QARP RECEIPT TEMPLATE</title>
+      <style>
+         @font-face {
+         font-family: 'willow bloom';
+         src: url('/public/material/frontend/assets/fonts/willowbloom-Regular.woff2') format('woff2'),
+         url('/public/material/frontend/assets/fonts/willowbloom-Regular.woff') format('woff');
+         font-weight: normal;
+         font-style: normal;
+         font-display: swap;
+         }
+         @font-face {
+         font-family: 'willow bloom';
+         src: url('/public/material/frontend/assets/fonts/willowbloom-Regular.woff2') format('woff2'),
+         url('/public/material/frontend/assets/fonts/willowbloom-Regular.woff') format('woff');
+         font-weight: normal;
+         font-style: normal;
+         font-display: swap;
+         }
+         .signature {
+         font-family: 'willow bloom';
+         }
+         body {
+         margin: 0 auto;
+         font-family: Montserrat, sans-serif;
+         }
+         table,
+         tr,
+         td {
+         border-collapse: collapse;
+         padding: 0;
+         font-size: 13px;
+         line-height: 20px;
+         margin: 0;
+         }
+         table,
+         tr,
+         td p {
+         font-size: 13px;
+         line-height: 20px;
+         padding: 0;
+         margin: 0;
+         }
+         @page {
+         @bottom-left-corner {
+         content: "Page " counter(page);
+         }
+         }
+         @media print {
+         /* avoid cutting tr's in half */
+         th div,
+         td div {
+         margin-top: -8px;
+         padding-top: 8px;
+         page-break-inside: avoid;
+         }
+         }
+         @media print {
+         table tr {
+         -webkit-print-color-adjust: exact;
+         print-color-adjust: exact;
+         }
+         }
+      </style>
+   </head>
+   <body>
+      <table style="width: 100%;padding-bottom: 10px;">
+         <tbody>
             <tr>
-                <td colspan="2">
-                    <p style="font-size: 11px; width: 80%;"><strong>RETURN FORM <span><u>ERT-N0924-02:</u></span></strong></p>
-                    <p style="font-size: 12px; line-height: 2; width: 80%;">A <span><u>digitized form of a</u></span> <strong>hand-signed</strong> copy of this NELP must be uploaded to  (<span style="background: yellow;">xxxx</span>).</p>
-                    <h5>NON-EXCLUSIVE LICENSE TO PUBLISH (“NELP”)</h5>
-                </td>
-                <td>
-                    <img src="logo.png" alt="" srcset="" style="width: 100px; float: right">
-                </td>
+               <td >
+                  <img style="max-width: 100%;width: 65%;" src="data:image/svg+xml;base64,<?php echo base64_encode(file_get_contents(base_path('public/uploads/' . $generalSetting->site_logo))); ?>" alt="<?=$generalSetting->site_name?>">
+               </td>
+               <td >
+                  <p style="font-weight: 700;padding-top:20px;font-size: 45px;padding-bottom: 20px;color:#2e3092;line-height: 56px;text-align:end;">
+                     INVOICE
+                  </p>
+               </td>
             </tr>
             <tr>
-                <td width="100%">
-                    <p style="line-height: 1.5; width: 100%; margin-top: 10px;">This NELP records the terms under which the Creative-Work specified below will be published online only on <strong>Ecosymbionts Regenerate Together</strong> (the “<strong>Platform</strong>”). The Platform is published by Ecosymbionts Regenerate (the “Publisher”). The Platform is owned by the Śramani Institute (the “<strong>Proprietor</strong>”).</p>
-                    <p style="width: 100%">Submission Reference Number (“<strong>SRN</strong>”): <span style="background: yellow;">xxx</span></p>
-                </td>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;font-weight:600">
+                     Invoice to :
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                     Invoice No : #<?=$invoice_no?>
+                  </p>
+                  <p style="font-size: 18px;color:#181717;">
+                     Date : <?=date_format(date_create($payment_date_time), "M d, Y h:i A")?>
+                  </p>
+               </td>
             </tr>
-            <!-- THE CREATIVE-WORK -->
-
             <tr>
-                <td style="width: 100%; border: 1px solid #000; background-color: #e7e6e6; padding: 5px 15px;" colspan="4">
-                    <p>THE CREATIVE-WORK</p>
-                </td>
+               <td style="">
+                  <p style="font-weight: 700;padding-top: 10px;font-size: 20px;padding-bottom: 20px;color: #2e3092;line-height: 26px;">
+                     <?=(($getUser)?$getUser->first_name . '' . $getUser->last_name:'')?>
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                  </p>
+               </td>
             </tr>
             <tr>
-                <td width="20%"><p style="border: 1px solid #000; border-top: none; background: #e7e6e6; padding: 5px 15px;">Creative-Work Title:</p></td>
-                <td width="80%"><p style="border: 1px solid #000; border-top: none; border-left: none; padding: 5px 15px;">[14 (note: this indicates the question number on the submission form; the answer should be pasted here)]<span style="display: block; margin-top: 20px; text-align: right;">(the <strong>“Contribution”</strong>)</span></p></td>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 15px;">
+                     <!-- [Address] -->
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-weight: 700;font-size: 20px;color: #2e3092;line-height: 26px;">
+                     ____
+                  </p>
+               </td>
             </tr>
-
-        </tbody>
-    </table>
-</body>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;">
+                     <!-- [Unit #] -->
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;">
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                  </p>
+               </td>
+               <td style="">
+                  <p style="font-weight: 700;padding-top: 10px;font-size: 20px;padding-bottom: 20px;color: #2e3092;line-height: 26px;text-align:end;">
+                     <!-- DUE : 1,200 -->
+                  </p>
+               </td>
+            </tr>
+            <tr style="background:#2e3092;">
+               <td >
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 15px;padding-top: 15px;color:#fff;padding-left:15px;font-weight:600;">
+                     Description
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 15px;padding-top: 15px;color:#fff;padding-right:15px;font-weight:600;">
+                     Total
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-left:15px;padding-bottom: 30px;padding-top: 30px;">
+                     <?=$particulars?>
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 30px;padding-top: 30px;padding-right:15px;">
+                     $<?=number_format($net_amount,2)?>
+                  </p>
+               </td>
+            </tr>
+            <!-- <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-left:15px;">
+                     Additional Person/Pet
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-right:15px;">
+                     $200
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-left:15px;padding-bottom: 30px;padding-top: 30px;">
+                     Late Fee
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 30px;padding-top: 30px;padding-right:15px;">
+                     $100
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-left:15px;">
+                     Administrative Fee
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-right:15px;">
+                     $50
+                  </p>
+               </td>
+            </tr> -->
+            <tr style="text-align: left;">
+               <td colspan="2" style="padding-bottom:0px;border-bottom:2px solid#2e3092;padding-bottom: 15px;padding-top: 15px;">
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;padding-top: 30px;font-weight:600;">
+                     
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-right:15px;padding-bottom: 30px;padding-top: 30px;padding-right:15px;">
+                     Sub-total : &nbsp;&nbsp;&nbsp;$<?=number_format($net_amount,2)?>
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;padding-top: 30px;font-weight:600;">
+                     Contact us:
+                  </p>
+               </td>
+               <td style="text-align: end;">
+                  <p style="font-size: 18px;color:#181717;padding-right:15px;padding-bottom: 30px;padding-top: 30px;padding-right:15px;">
+                     Discount : &nbsp;&nbsp;&nbsp;$<?=number_format($discount_amount,2)?>
+                  </p>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                     <?=$generalSetting->site_name?>
+                  </p>
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                     Phone Number: <?=$generalSetting->site_phone?>
+                  </p>
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                     Email: <?=$generalSetting->system_email?>
+                  </p>
+                  <p style="font-size: 18px;color:#181717;padding-bottom: 10px;">
+                     <?=$generalSetting->site_url?>
+                  </p>
+               </td>
+               <td align="right" >
+                  <table style="width:100%;">
+                     <tbody>
+                        <tr class="bg" style="width: 100%;background: #2e3092;line-height: 20px; color:#fff;">
+                           <td style="width:50%;padding: 10px 25px 10px 25px;">
+                              <p style="font-size: 18px;color:#fff;font-weight: 600;">Total : </p>
+                           </td>
+                           <td style="width:50%;padding: 10px 25px 10px 25px;">
+                              <p style="font-size: 18px;color:#fff;font-weight: 600;">$<?=number_format($payment_amount,2)?></p>
+                           </td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </td>
+            </tr>
+            <tr>
+               <td style="">
+                  <p style="font-size: 18px;color:#181717;font-weight:600;">
+                     Thank You for Being a Valued Tenant!
+                  </p>
+               </td>
+               <td  >
+                  <p style="text-align: center;font-weight: 700;font-size: 20px;padding-bottom: 5px;color: #2e3092;line-height: 26px;">
+                     KARLA<br>
+                     HERRERA
+                  </p>
+                  <p style="text-align: center;font-size: 18px;color:#181717;padding-top: 10px;">
+                     Administrator
+                  </p>
+               </td>
+            </tr>
+         </tbody>
+      </table>
+   </body>
 </html>
