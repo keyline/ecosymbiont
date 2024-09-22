b4cc49683d81893d50df2047ddc5e0d721fe27d5
surajit bera <surajit.b@keylines.net>
2024-09-22 09:55:08 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -60,7 +60,7 @@ $generalSetting            = GeneralSetting::find('1');
     <table style="width: 100%; background: #fff; padding: 10px;">
         <tbody>
             <tr>
-                <td  width="100%" colspan="2">
+                <td  width="100%">
                     <p style="font-size: 11px;"><strong>RETURN FORM <span><u>ERT-N0924-02:</u></span></strong></p>
                 </td>
                 <td align="right" rowspan="3">
@@ -68,15 +68,15 @@ $generalSetting            = GeneralSetting::find('1');
                 </td>
             </tr>
             <tr>
-                <td  width="100%" colspan="2">
+                <td  width="100%">
                     <p style="font-size: 12px; line-height: 2;">A <span><u>digitized form of a</u></span> <strong>hand-signed</strong> copy of this NELP must be uploaded to  (<span style="background: yellow;">xxxx</span>).</p>
                 </td>
             </tr>
             <tr>
-                <td  width="100%" colspan="2"><h5>NON-EXCLUSIVE LICENSE TO PUBLISH (“NELP”)</h5></td>
+                <td  width="100%"><h5>NON-EXCLUSIVE LICENSE TO PUBLISH (“NELP”)</h5></td>
             </tr>
             <tr>
-                <td width="100%" colspan="2">
+                <td width="100%" >
                     <p style="line-height: 1.5; width: 100%; margin-top: 10px;">This NELP records the terms under which the Creative-Work specified below will be published online only on <strong>Ecosymbionts Regenerate Together</strong> (the “<strong>Platform</strong>”). The Platform is published by Ecosymbionts Regenerate (the “Publisher”). The Platform is owned by the Śramani Institute (the “<strong>Proprietor</strong>”).</p>
                     <p style="width: 100%">Submission Reference Number (“<strong>SRN</strong>”): <span style="background: yellow;">xxx</span></p>
                 </td>
