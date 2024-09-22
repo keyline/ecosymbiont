fbae05d43368cf1dc8f3127fcc3a385748a0f43d
subhomoy samanta <subhomoy@keylines.net>
2024-09-22 21:12:33 +0530

    client feedback point in frontend

--- resources/views/admin/maincontents/article/list.blade.php
+++ resources/views/admin/maincontents/article/list.blade.php
@@ -73,10 +73,10 @@ $controllerRoute = $module['controller_route'];
                       ?>
                     </td>
                     <td>
-                      <?php if ($row->is_published == '0' || $row->is_published == '1') { ?>
+                      <?php if ($row->is_published == 3) { ?>
                         <a href="<?=url('admin/' . $controllerRoute . '/change_status_accept/'.Helper::encoded($row->id))?>" class="btn btn-success btn-sm" title="Accept <?=$module['title']?>">Accept</a>
                         <a href="<?=url('admin/' . $controllerRoute . '/change_status_reject/'.Helper::encoded($row->id))?>" class="btn btn-danger btn-sm" title="Reject <?=$module['title']?>">Reject</a>
-                     <?php } elseif($row->is_published == '2'){?>
+                     <?php } elseif($row->is_published == 4){?>
                         <a href="<?=url('admin/' . $controllerRoute . '/change_status_reject/'.Helper::encoded($row->id))?>" class="btn btn-danger btn-sm" title="Reject <?=$module['title']?>">Reject</a>
                      <?php } else{ ?>
                         <a href="<?=url('admin/' . $controllerRoute . '/change_status_accept/'.Helper::encoded($row->id))?>" class="btn btn-success btn-sm" title="Accept <?=$module['title']?>">Accept</a>
