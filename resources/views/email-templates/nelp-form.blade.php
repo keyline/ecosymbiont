3d0658658b5dcf7f64d540d9eca4cc8b06ddd58e
surajit bera <surajit.b@keylines.net>
2024-09-22 09:19:32 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -96,13 +96,13 @@ $generalSetting            = GeneralSetting::find('1');
             <table width="100%">
                 <tbody>
                     <tr>
-                        <td  style="width: 100%; border: 1px solid #000; border-top: none; border-left: none; padding: 5px 15px; background-color: grey;">THE CREATIVE-WORK</td>
+                        <td colspan="2" style="border: 1px solid #000; border-top: none; border-left: none; padding: 5px 15px; background-color: grey;">THE CREATIVE-WORK</td>
                     </tr>
                     <tr>
                         <td>
                             <p style="border: 1px solid #000; border-top: none; background: #e7e6e6; padding: 5px 15px; ">Creative-Work Title:</p>
                         </td>
-                        <td><p style="border: 1px solid #000; border-top: none; border-left: none; padding: 5px 15px; background-color: grey;">[14 (note: this indicates the question number on the submission form; the answer should be pasted here)]<span style="display: block; margin-top: 20px; text-align: right;">(the <strong>“Contribution”</strong>)</span></p></td>
+                        <td><p style="border: 1px solid #000; border-top: none; border-left: none; padding: 5px 15px; background-color: grey;">[14 (note: this indicates the question number on the submission form; the answer should be pasted here)]<span style="margin-top: 20px; text-align: right;">(the <strong>“Contribution”</strong>)</span></p></td>
                     </tr>
                 </tbody>
             </table>
