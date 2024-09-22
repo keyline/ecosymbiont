ddc2d9dbcb5cbda859d091fd175cfa254f68ef14
surajit bera <surajit.b@keylines.net>
2024-09-22 14:02:35 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -273,6 +273,9 @@ $generalSetting            = GeneralSetting::find('1');
             </tr>
         </tbody>
     </table>
-
+    <ul>
+        <li style="font-weight: bold;">1. 	STANDARD TERMS AND CONDITIONS</li>
+        <li>1.1 	The Author hereby agrees to be bound by all terms and conditions in this NELP. </li>
+    </ul>    
 </body>
 </html>
