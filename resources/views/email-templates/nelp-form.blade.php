0e1f8af37695bfa1de0206f471f9a22704ce6d30
surajit bera <surajit.b@keylines.net>
2024-09-22 13:00:30 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -224,7 +224,11 @@ $generalSetting            = GeneralSetting::find('1');
                     <p style="color: #4472c4;"><i>You must check this box and enter details, if applicable</i></p>
                 </td>
                 <td width="70%" colspan="6" valign="top" style="border: 1px solid #000; padding: 5px 15px;">
-                    <p>One or more Authors are employed by the Śramani Institute or are related to a Śramani Institute employee. Please provide names and describe the relationship(s):</p>
+                    <div  style="padding-bottom: 10px;">
+                     <input type="checkbox"  name="" id="" style="height:21px; width:28px; vertical-align: middle;margin: 0;margin-top: 0px;">
+                        <label style="margin: 0;padding-left: 3px;font-weight: normal;vertical-align: middle;font-size: 18px;color: #2e3192;">One or more Authors are employed by the Śramani Institute or are related to a Śramani Institute employee. Please provide names and describe the relationship(s):
+                        </label>
+                    </div>
                 </td>
             </tr>
         </tbody>
