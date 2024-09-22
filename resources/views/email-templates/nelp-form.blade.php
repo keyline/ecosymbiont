8a6876326f6521b2489f088549b5849ce69a1f95
surajit bera <surajit.b@keylines.net>
2024-09-22 10:41:16 +0530

    pdf edit

--- resources/views/email-templates/nelp-form.blade.php
+++ resources/views/email-templates/nelp-form.blade.php
@@ -110,8 +110,8 @@ $generalSetting            = GeneralSetting::find('1');
                 </td>
                 <td>&nbsp;&nbsp;&nbsp;</td>
             </tr>
-            <tr>
-            <td width="30%" style="border: 1px solid #000; border-top: none; background: #e7e6e6; padding: 5px 15px;">Creative-Work Title:</td>
+            <tr  style="border: 1px solid #000;">
+                <td width="30%" style="border: 1px solid #000; border-top: none; background: #e7e6e6; padding: 5px 15px;">Creative-Work Title:</td>
                 <td width="100%">
                     <p style="border: 1px solid #000; border-top: none; padding: 5px 15px;">
                     [14 (note: this indicates the question number on the submission form; the answer should be pasted here)]<span style="display: block; margin-top: 20px; text-align: right;">(the <strong>“Contribution”</strong>)</span>
