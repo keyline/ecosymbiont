627c35b17a691a7efd5a1e89fe44b0cbe0dd0f3c
Deblina Das <deblina@keylines.net>
2024-09-23 15:32:20 +0530

    frontend panel 23-09-2024 chnges

--- include/header.php
+++ include/header.php
@@ -38,7 +38,7 @@ function isActive($page) {
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">                        
-                        <li><a class="<?= isActive('index') ?>" href="#">About</a></li>
+                        <li><a class="<?= isActive('index') ?>" href="index.php">About</a></li>
                         <li><a href="<?=$base_url?>/together">ERT</a></li>
                         <li><a class="<?= isActive('synergy-meetings') ?>" href="synergy-meetings.php">Synergy Meetings</a></li>                        
                         <li><a class="<?= isActive('in-the-media') ?>" href="in-the-media.php">In the Media</a></li>
