274a2fc167f75b5c79124d73d19436432aea9517
Deblina Das <deblina@keylines.net>
2024-09-24 18:05:06 +0530

    admin panel 24-09-2024 chnges

--- contact.php
+++ contact.php
@@ -28,18 +28,21 @@ include "include/header.php";
                                     <input type="test" class="form-control" id="">
                                 </div>
                             </div>
-                            <div class="form-group ">
+                            <div class="form-group col-md-6">
                                 <label class="form-check-label" for="exampleCheck1">Country of residence*</label>
-                                    <select class="form-control" name="field1" id="field1" multiple onchange="console.log(Array.from(this.selectedOptions).map(x=>x.value??x.text))" multiselect-hide-x="true">
-                                        <option value="1">I am interested in fuding the work of ER and/or ERT</option>
-                                        <option value="2">I am interested in learning more about the work of ER and/or ERT</option>
-                                        <option value="3">I am interested in submitting Creative-Work to ERT</option>
-                                    </select> 
+                                <input type="test" class="form-control" id=""> 
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="form-check-label" for="exampleCheck1">Subject*</label>
+                                <select class="form-control" name="field1" id="field1" multiple onchange="console.log(Array.from(this.selectedOptions).map(x=>x.value??x.text))" multiselect-hide-x="true">
+                                    <option value="1">I am interested in fuding the work of ER and/or ERT</option>
+                                    <option value="2">I am interested in learning more about the work of ER and/or ERT</option>
+                                    <option value="3">I am interested in submitting Creative-Work to ERT</option>
+                                </select> 
                             </div>
                             <div class="form-group ">
                                 <label for="exampleCheck1">Message (up to 250 words)*</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                               
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>                               
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
