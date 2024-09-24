9e615cd67229f9357f945a0d1f92f14608e49fd4
Deblina Das <deblina@keylines.net>
2024-09-24 18:06:45 +0530

    admin panel 24-09-2024 chnges

--- contact.php
+++ contact.php
@@ -19,26 +19,28 @@ include "include/header.php";
                     <div class="info-contact_form">
                         <form>
                             <div class="form-row">
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-6">
                                     <label for="exampleInputEmail1">Full name*</label>
                                     <input type="text" class="form-control" id="exampleInputEmail1">
                                 </div>
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-6">
                                     <label for="exampleInputPassword1">Email address*</label>
                                     <input type="test" class="form-control" id="">
                                 </div>
                             </div>
-                            <div class="form-group col-md-6">
-                                <label class="form-check-label" for="exampleCheck1">Country of residence*</label>
-                                <input type="test" class="form-control" id=""> 
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label class="form-check-label" for="exampleCheck1">Subject*</label>
-                                <select class="form-control" name="field1" id="field1" multiple onchange="console.log(Array.from(this.selectedOptions).map(x=>x.value??x.text))" multiselect-hide-x="true">
-                                    <option value="1">I am interested in fuding the work of ER and/or ERT</option>
-                                    <option value="2">I am interested in learning more about the work of ER and/or ERT</option>
-                                    <option value="3">I am interested in submitting Creative-Work to ERT</option>
-                                </select> 
+                            <div class="form-row">
+                                <div class="form-group col-6">
+                                    <label class="form-check-label" for="exampleCheck1">Country of residence*</label>
+                                    <input type="test" class="form-control" id=""> 
+                                </div>
+                                <div class="form-group col-6">
+                                    <label class="form-check-label" for="exampleCheck1">Subject*</label>
+                                    <select class="form-control" name="field1" id="field1" multiple onchange="console.log(Array.from(this.selectedOptions).map(x=>x.value??x.text))" multiselect-hide-x="true">
+                                        <option value="1">I am interested in fuding the work of ER and/or ERT</option>
+                                        <option value="2">I am interested in learning more about the work of ER and/or ERT</option>
+                                        <option value="3">I am interested in submitting Creative-Work to ERT</option>
+                                    </select> 
+                                </div>
                             </div>
                             <div class="form-group ">
                                 <label for="exampleCheck1">Message (up to 250 words)*</label>
