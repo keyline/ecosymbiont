e44fc76fcb8593070db30b8835595444dea4bb4e
debojyti <debojyti@keylines.net>
2024-09-24 22:21:13 +0530

    fixed all 240924-2

--- together/resources/views/front/elements/header.blade.php
+++ together/resources/views/front/elements/header.blade.php
@@ -81,13 +81,14 @@ $details = json_decode($details);
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
+                    <a class="navbar-brand" href="<?=url('/')?>"><img src="<?=env('UPLOADS_URL').$generalSetting->site_logo?>" alt="<?=$generalSetting->site_name?>"></a>
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?=url('/')?>"><img src="<?=env('UPLOADS_URL').$generalSetting->site_logo?>" alt="<?=$generalSetting->site_name?>"></a>
+                    
                 </div>
                 
             </div>
