532e85f106600eebf57e026d5fdb69cc0ee3f2ac
debojyti <debojyti@keylines.net>
2024-09-25 12:50:18 +0530

    fixed all 250924-1

--- together/resources/views/front/elements/footer.blade.php
+++ together/resources/views/front/elements/footer.blade.php
@@ -28,7 +28,7 @@ use App\Helpers\Helper;
                 <div class="col-md-12">
                     <div class="foot_copyinfo"><?=$generalSetting->footer_description?></div>
                     <div class="kelfot">
-                        <a href="https://keylines.net/" target="_blank">Powered by keylines.net</a>
+                        <a href="https://keylines.net/" target="_blank">Powered by Keylines</a>
                     </div>
                 </div>
             </div>
@@ -38,13 +38,13 @@ use App\Helpers\Helper;
 <div class="container">
     <div class="footer-widgets-part">
         <div class="row">
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="widget text-widget">
                     <h1>About</h1>
                     <p></p>
                     <a href="index.html"><img src="images/logo.png" alt=""></a>
                 </div>
-            </div>
+            </div> -->
             <!-- <div class="col-md-3">
                 <div class="widget tags-widget">
                     <h1>Popular Tags</h1>
@@ -62,7 +62,7 @@ use App\Helpers\Helper;
                     </ul>
                 </div>
             </div> -->
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="widget posts-widget">
                     <h1>Random Content</h1>
                     <ul class="list-posts">
@@ -88,7 +88,7 @@ use App\Helpers\Helper;
                         <?php } }?>
                     </ul>
                 </div>
-            </div>
+            </div> -->
             <!-- <div class="col-md-3">
                 <div class="widget flickr-widget">
                     <h1>Flickr Photos</h1>
@@ -105,7 +105,7 @@ use App\Helpers\Helper;
             </div> -->
         </div>
     </div>
-    <div class="footer-last-line">
+    <!-- <div class="footer-last-line">
         <div class="row">
             <div class="col-md-6">
                 <p><?=$generalSetting->footer_text?></p>
@@ -115,5 +115,5 @@ use App\Helpers\Helper;
                 
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
