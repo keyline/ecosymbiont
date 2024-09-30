d3dcd7abcf3081f41bec1bc3ad14d2b08f6743db
debojyti <debojyti@keylines.net>
2024-09-30 20:19:01 +0530

    fixed all 300924

--- together/resources/views/front/pages/home.blade.php
+++ together/resources/views/front/pages/home.blade.php
@@ -7,6 +7,11 @@ use App\Helpers\Helper;
 <!-- heading-news-section2 ================================================== -->
     <section class="heading-news2">
         <div class="container">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6"></div>
+                <div class="col-md-3"></div>
+            </div>
             <div class="iso-call heading-news-box">
                 <?php
                 $parentCategoryContent1 = NewsContent::join('news_category as parent_category', 'news_contents.parent_category', '=', 'parent_category.id') // Join for parent category
