3800269eeb0a8190298fb661b57130797b410acf
subhomoy samanta <subhomoy@keylines.net>
2024-10-22 11:06:09 +0530

    search bar design fixing

--- earth/resources/views/front/pages/submissions.blade.php
+++ earth/resources/views/front/pages/submissions.blade.php
@@ -183,14 +183,14 @@
                                                 <h2>COPYRIGHT & LICENSE</h2>
                                                 <p class="black">As per the terms detailed in the NELP, you grant EaRTh, ER, and the Śramani Institute (the nonprofit host organization of ER and EaRTh) a non-exclusive license to publish and use your Content. You will retain the copyright to your work and the right to also publish elsewhere. </p>
                                             </div>
-                                            <!-- <a class="btn btn-primary blue_btn" href="#">SUBMIT CREATIVE-WORK</a> -->
-                                            <?php if($button_show){?>
+                                            <p class="text-center"><a href="<?=url()?>contact.php" class="btn btn-primary"><i class="fa fa-plus-circle"></i> Submit New Creative-Work</a></p>
+                                            <!-- <?php if($button_show){?>
                                                 <?php if(session('is_user_login')){?>
                                                     <p class="text-center"><a href="<?=url('user/submit-new-article')?>" class="btn btn-primary"><i class="fa fa-plus-circle"></i> Submit New Creative-Work</a></p>
                                                 <?php } else {?>
                                                     <p class="text-center"><a href="<?=url('signin')?>" class="btn btn-primary">Submit New Creative-Work</a></p>
                                                 <?php }?>
-                                            <?php }?>
+                                            <?php }?> -->
                                         </div>
                                     </div>
                                 </div>
