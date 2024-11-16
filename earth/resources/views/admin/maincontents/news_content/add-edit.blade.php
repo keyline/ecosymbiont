97b1274386626b48e6d1f3a60a37733f623e91e9
Deblina Das <deblina@keylines.net>
2024-11-16 15:05:04 +0530

    csrf token fixing

--- earth/resources/views/admin/maincontents/news_content/add-edit.blade.php
+++ earth/resources/views/admin/maincontents/news_content/add-edit.blade.php
@@ -606,7 +606,7 @@ $controllerRoute = $module['controller_route'];
         </div>
     </div>
 </section>
-<script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+<!-- <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
