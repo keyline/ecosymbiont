dfd307e90754983ee0738cf6e92b3c5bd7fefa70
Deblina Das <deblina@keylines.net>
2024-12-16 16:42:48 +0530

    all profile updation done

--- earth/resources/views/front/pages/user/submit-new-article.blade.php
+++ earth/resources/views/front/pages/user/submit-new-article.blade.php
@@ -148,6 +148,7 @@
             </div>
             <div class="col-md-12">
                 <div class="card">
+                    <h3>Form: Creative-work</h3>
                     <div class="card-body">
                         <form method="POST" id="saveForm" action="" enctype="multipart/form-data" oninput="validateForm()">
                         <!-- <input type="hidden" name="form_action" id="formAction"> -->
