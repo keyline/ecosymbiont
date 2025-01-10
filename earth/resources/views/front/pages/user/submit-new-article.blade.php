3e0e14fed83db23557d0a06eb754c98373fa8919
Deblina Das <deblina@keylines.net>
2025-01-10 14:48:40 +0530

    feedback fixing

--- earth/resources/views/front/pages/user/submit-new-article.blade.php
+++ earth/resources/views/front/pages/user/submit-new-article.blade.php
@@ -15,7 +15,7 @@
         color: #fff;
     }
     .blue-text{
-        color: #0075ff;
+        color: #6187ce;
     }
 </style>
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
