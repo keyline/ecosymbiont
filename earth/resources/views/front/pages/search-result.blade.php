6c8d154302b680d6e9412da617ef9c48bd1d91a6
Deblina Das <deblina@keylines.net>
2025-02-07 20:15:39 +0530

    client feedback.

--- earth/resources/views/front/pages/search-result.blade.php
+++ earth/resources/views/front/pages/search-result.blade.php
@@ -40,11 +40,11 @@ $current_url = $protocol . $host . $uri;
                                             <?php } else {?>
                                                 <div class="post-gallery video-post">
                                                     <img alt="" src="https://img.youtube.com/vi/<?=$rowContent->videoId?>/hqdefault.jpg">
-                                                    <?php if(session('is_user_login')){?>
+                                                    <!-- ?php if(session('is_user_login')){?> -->
                                                         <a href="https://www.youtube.com/watch?v=<?=$rowContent->videoId?>" class="video-link"><i class="fa fa-play-circle-o"></i></a>
-                                                    <?php } else {?>
-                                                        <a href="<?=url('sign-in/' . Helper::encoded($current_url))?>" class="video-link-without-signin"><i class="fa fa-play-circle-o"></i></a>
-                                                    <?php }?>
+                                                    <!-- ?php } else {?> -->
+                                                        <!-- <a href="?=url('sign-in/' . Helper::encoded($current_url))?>" class="video-link-without-signin"><i class="fa fa-play-circle-o"></i></a> -->
+                                                    <!-- ?php }?> -->
                                                 </div>
                                             <?php } ?>
                                         </div>
