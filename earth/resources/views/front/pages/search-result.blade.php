2a4d4396032764ead30b22edb640fba6fb0cd488
Deblina Das <deblina@keylines.net>
2025-02-11 15:32:13 +0530

    client feedback.

--- earth/resources/views/front/pages/search-result.blade.php
+++ earth/resources/views/front/pages/search-result.blade.php
@@ -299,7 +299,7 @@ $current_url = $protocol . $host . $uri;
     $('#load_more_btn').on('click', function () {
         $('#loading').show();
         $.ajax({
-            url: '<?= url('search-result') ?>',
+            url: '<?= url('search_result_load') ?>',
             type: 'POST',
             data: {
                 offset: offset,
