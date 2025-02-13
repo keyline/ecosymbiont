ad9b2d974fe05b8a00c974463d891ca0055fa713
debojyti <debojyti@keylines.net>
2025-02-13 16:40:09 +0530

    maya13025

--- earth/resources/views/front/pages/SchumacherWild.blade.php
+++ earth/resources/views/front/pages/SchumacherWild.blade.php
@@ -13,8 +13,10 @@
                 <div class="block-content">
                     <div class="article-box">                            
                         <div class="titleto-box">
-                            <h1>SCHUMACHER WILD</h1>  
-                            <p class="black">We appreciate your patience as we develop the <em><strong>Virtual Community Center</strong></em> for Schumacher Wild.</p>                                                        
+                            <h1>SCHUMACHER WILD NETWORK</h1>  
+                            <p class="black"><a href="https://www.schumacherwild.org/" target="_blank" style="color: #d09c1c;">Schumacher Wild</a> is a reimagination of Schumacher College, with a new curriculum fit for the 21st century being co-created. We offer a radically new form of learning. We are a transdisciplinary college. Transdisciplinarity, for us, means to create and participate in a continuous, unfolding dialogue between the disciplines: between ecology and poetry, between economics and phenomenology, between design and mythology, between soil science and contemplative practice. In short, we offer an interwoven, interfused, intertwined, deeply polyphonic way of learning.</p>
+                            <p class="black">Schumacher Wild is supported by the <a href="https://www.satishkumarfoundation.co.uk/" target="_blank" style="color: #d09c1c;">Satish Kumar Foundation</a>.</p>     
+                            <p class="black">We appreciate your patience as we develop the <strong><em>Virtual Community Center</em></strong> for the Schumacher Wild Network.</p>   
                         </div>                           
                     </div>
                 </div>
