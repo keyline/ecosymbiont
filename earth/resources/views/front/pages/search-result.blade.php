adf000fe9afad557ef8f552e9baf5da0053528d3
subhomoy samanta <subhomoy@keylines.net>
2025-02-14 15:09:27 +0530

    search functionality bug fixing

--- earth/resources/views/front/pages/search-result.blade.php
+++ earth/resources/views/front/pages/search-result.blade.php
@@ -326,9 +326,9 @@ $current_url = $protocol . $host . $uri;
                 // }
                 if (contents.length > 0) {
                     let contentHtml = '';
-                    $('.video-link').magnificPopup({
-                        type: 'iframe'
-                    });
+                    // $('.video-link').magnificPopup({
+                    //     type: 'iframe'
+                    // });
                     contents.forEach(content => {
                         contentHtml += `
                             <div class="news-post article-post">
@@ -371,6 +371,26 @@ $current_url = $protocol . $host . $uri;
                                     </div>
                                 </div>
                             </div>`;
+
+                            $(document).on("click", ".video-link", function(e) {
+                                e.preventDefault();
+                                var videoUrl = $(this).attr("href");
+
+                                // Open YouTube video in a popup (you can customize this as needed)
+                                var popupHtml = `<div class="video-popup">
+                                                    <div class="video-popup-content">
+                                                        <iframe width="560" height="315" src="${videoUrl.replace("watch?v=", "embed/")}" frameborder="0" allowfullscreen></iframe>
+                                                        <span class="close-popup">×</span>
+                                                    </div>
+                                                 </div>`;
+
+                                $("body").append(popupHtml);
+
+                                // Close popup on click
+                                $(".close-popup").on("click", function() {
+                                    $(".video-popup").remove();
+                                });
+                            });
                     });
                     $('#content-list').append(contentHtml);
                     offset += contents.length;
@@ -391,23 +411,5 @@ $current_url = $protocol . $host . $uri;
         });
     });
     
-    $(document).on("click", ".video-link", function(e) {
-        e.preventDefault();
-        var videoUrl = $(this).attr("href");
 
-        // Open YouTube video in a popup (you can customize this as needed)
-        var popupHtml = `<div class="video-popup">
-                            <div class="video-popup-content">
-                                <iframe width="560" height="315" src="${videoUrl.replace("watch?v=", "embed/")}" frameborder="0" allowfullscreen></iframe>
-                                <span class="close-popup">×</span>
-                            </div>
-                         </div>`;
-
-        $("body").append(popupHtml);
-
-        // Close popup on click
-        $(".close-popup").on("click", function() {
-            $(".video-popup").remove();
-        });
-    });
 </script>
