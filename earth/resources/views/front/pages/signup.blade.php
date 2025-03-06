68dd6726209d6c43d5150765abceec4a8bd33cef
Deblina Das <deblina@keylines.net>
2025-03-06 12:10:11 +0530

    captche reset for dev

--- earth/resources/views/front/pages/signup.blade.php
+++ earth/resources/views/front/pages/signup.blade.php
@@ -138,10 +138,19 @@
       });
     </script>
 <!-- End block-wrapper-section -->
-<script src="https://www.google.com/recaptcha/api.js?render=6LcIw04qAAAAAGBE5JP7v7i3gYEa4OPNSWqBlvbH"></script>
+<!-- <script src="https://www.google.com/recaptcha/api.js?render=6LcIw04qAAAAAGBE5JP7v7i3gYEa4OPNSWqBlvbH"></script>
 <script>
 grecaptcha.ready(function() {
     grecaptcha.execute('6LcIw04qAAAAAGBE5JP7v7i3gYEa4OPNSWqBlvbH', {action: 'submit'}).then(function(token) {
+        // Add the token to your form submission
+        document.getElementById('g-recaptcha-response').value = token;
+    });
+});
+</script> -->
+<script src="https://www.google.com/recaptcha/api.js?render=6Ldum88qAAAAAGgaGIGZqvD0cZP_KzBWgN9CRUYO"></script>
+<script>
+grecaptcha.ready(function() {
+    grecaptcha.execute('6Ldum88qAAAAAGgaGIGZqvD0cZP_KzBWgN9CRUYO', {action: 'submit'}).then(function(token) {
         // Add the token to your form submission
         document.getElementById('g-recaptcha-response').value = token;
     });
