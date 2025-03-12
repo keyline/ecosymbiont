1fd0776345d1df9dc3bfca98d18cd4d50a5f7335
subhomoy samanta <subhomoy@keylines.net>
2025-03-12 14:14:25 +0530

    advance search

--- earth/resources/views/front/elements/footer.blade.php
+++ earth/resources/views/front/elements/footer.blade.php
@@ -130,8 +130,8 @@ use App\Helpers\Helper;
                 </button>
                 <h4 class="modal-title" id="modalLabel">Advanced Search</h4>
             </div>
+                <form class="navbar-form" method="GET" action="<?=url('search-result')?>" role="search">
             <div class="modal-body">
-                <!-- <form class="navbar-form" method="GET" action="<?=url('search-result')?>" role="search"> -->
                     <div class="advance_inner_search">
                         <select id="search_type" class="adv-search-field field-selector" name="search_type" required>
                             <option value="All Fields" selected="selected">All Fields</option>
@@ -146,10 +146,10 @@ use App\Helpers\Helper;
                             <option value="Text">Text</option>
                         </select>
                         <input type="search" id="search_keyword" name="search_keyword" placeholder="Enter a search term" required>
-                        <button type="submit" class="btn btn-default" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search</button>
+                        <button type="submit" class="btn btn-default" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-search"></i> Search</button>
                     </div>
-                <!-- </form> -->
             </div>
+                </form>
         </div>
     </div>
 </div>
