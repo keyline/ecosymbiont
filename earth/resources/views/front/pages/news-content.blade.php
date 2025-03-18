7cb90c1b9348d886f125f2cbd844029aaf4c4994
Deblina Das <deblina@keylines.net>
2025-03-18 13:26:20 +0530

    citation new

--- earth/resources/views/front/pages/news-content.blade.php
+++ earth/resources/views/front/pages/news-content.blade.php
@@ -14,6 +14,8 @@ $host = $_SERVER['HTTP_HOST'];
 $uri = $_SERVER['REQUEST_URI'];
 $current_url = $protocol . $host . $uri;
 ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <!-- block-wrapper-section ================================================== -->
     <section class="block-wrapper">
