5982a0a04e90af74b7ac4fa655f73b26a65ff7b7
Deblina Das <deblina@keylines.net>
2025-03-19 17:08:45 +0530

    citation new

--- earth/app/Http/Controllers/FrontController.php
+++ earth/app/Http/Controllers/FrontController.php
@@ -414,8 +414,8 @@ class FrontController extends Controller
                 $data['keyword']    = $search_keyword;
             } elseif($search_type == 'Community'){
                 $search_keyword     = $postData['search_keyword3'];
-                $getCommunity       = Community::select('name')->where('id', '=', $search_keyword)->first();
-                $title              = 'Search result for: "' . (($getCommunity)?$getCommunity->name:'') . '" ('.$search_type.')';
+                // $getCommunity       = Community::select('name')->where('id', '=', $search_keyword)->first();
+                $title              = 'Search result for: "' . ($search_keyword) . '" ('.$search_type.')';
                 $data['keyword']    = $search_keyword;
             }else {
                 $search_keyword     = $postData['search_keyword0'];
