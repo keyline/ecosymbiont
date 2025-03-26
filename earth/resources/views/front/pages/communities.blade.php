768aaa3f3a72eecf32cdc81db9655a6cb0b3930f
surajit bera <surajit.b@keylines.net>
2025-03-26 16:51:17 +0530

    comminuti page content upload

--- earth/resources/views/front/pages/communities.blade.php
+++ earth/resources/views/front/pages/communities.blade.php
@@ -14,16 +14,27 @@
                     <div class="article-box">                            
                         <div class="titleto-box">
                             <h1>COMMUNITIES</h1>  
-                            <p class="black">As part of its <a target="_blank" href="<?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p>
-                            <p class="black">We appreciate your patience as we develop this feature.</p>
-                            <p class="black">Currently, we are working with the communities below.</p>
+                            {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
+                            {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
+                            <p class="black">Currently, we are working with the communities below.</p> --}}
+                            {{-- <div class="titleto-inner">
+                                <ul>
+                                    <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
+                                    <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
+                                    <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
+                                    <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
+                                    
+                                </ul>
+                            </div>
+                            <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
+                            <p><strong>EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
+                            <p><strong>EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
+                            <h5 style="padding: 10px; border: 1px solid #000;">But that’s not all EaRTh is.</h5>
+                            <p>Through <strong><i>EaRTh Communities</i></strong>, we help members of Communities (whether formed based on geography or like-minded collaboration) to:</p>
                             <div class="titleto-inner">
                                 <ul>
-                                    <li><a target="_blank" href="<?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
-                                    <li><a target="_blank" href="<?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="<?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="<?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
-                                    
+                                    <li><strong><i>weave</i></strong> with each other, by securely connecting to share ideas, experiences, and knowledge</li>
+                                    <li><strong><i>thrive</i></strong> together, by co-creating and co-implementing ecology-rooted livelihoods and innovations</li>
                                 </ul>
                             </div>
                             <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="<?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p>
