79a0f535d6b99f6e2d1bd1121fc50e65aaa1ae26
surajit bera <surajit.b@keylines.net>
2025-03-26 16:53:52 +0530

    comminuti page content upload

--- earth/resources/views/front/pages/communities.blade.php
+++ earth/resources/views/front/pages/communities.blade.php
@@ -13,7 +13,7 @@
                 <div class="block-content">
                     <div class="article-box">                            
                         <div class="titleto-box">
-                            <h1>COMMUNITIES</h1>  
+                            <h1>EaRTh COMMUNITIES</h1>  
                             {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
                             {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
                             <p class="black">Currently, we are working with the communities below.</p> --}}
@@ -29,7 +29,7 @@
                             <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
                             <p><strong>EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
                             <p><strong>EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
-                            <h5 style="padding: 10px; border: 1px solid #000;">But that’s not all EaRTh is.</h5>
+                            <h5 style="padding: 5px; border: 1px solid #000; text-align: center; font-weight: 500">But that’s not all EaRTh is.</h5>
                             <p>Through <strong><i>EaRTh Communities</i></strong>, we help members of Communities (whether formed based on geography or like-minded collaboration) to:</p>
                             <div class="titleto-inner">
                                 <ul>
