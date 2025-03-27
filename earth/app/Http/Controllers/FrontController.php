9c04d52dc6a1ec5c7acc4a87556cafa2e1b49a7f
surajit bera <surajit.b@keylines.net>
2025-03-27 10:36:19 +0530

    projects page add

--- earth/app/Http/Controllers/FrontController.php
+++ earth/app/Http/Controllers/FrontController.php
@@ -182,6 +182,14 @@ class FrontController extends Controller
         $data['search_keyword']         = '';
         echo $this->front_before_login_layout($title, $page_name, $data);
     }
+    public function projects()
+    {
+        $data = [];
+        $title                          = 'Projects';
+        $page_name                      = 'Projects';
+        $data['search_keyword']         = '';
+        echo $this->front_before_login_layout($title, $page_name, $data);
+    }
     public function schumacherWild()
     {
         $data = [];
--- earth/resources/views/front/elements/header.blade.php
+++ earth/resources/views/front/elements/header.blade.php
@@ -256,7 +256,8 @@ use App\Helpers\Helper;
                                 </li>
                             <?php } }?>
                             <li><a href="<?=url('submissions')?>" style="color: black !important;">SUBMISSIONS</a></li>
-                            <li><a href="<?=env('REGENERATE_URL')?>contact.php" style="color: black !important;">CONTACT</a></li>
+                            {{-- <li><a href="?=env('REGENERATE_URL')?>contact.php" style="color: black !important;">CONTACT</a></li> --}}
+                            <li><a href="<?=url('projects')?>" style="color: black !important;">PROJECTS</a></li>
                         </ul>
                         <!-- desktop -->
                     </div>
