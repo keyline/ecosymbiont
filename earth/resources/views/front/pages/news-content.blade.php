68451c05cc4d263b59deb4213dd2658b6fab57a6
Deblina Das <deblina@keylines.net>
2025-03-28 18:26:13 +0530

    project modification

--- earth/resources/views/front/pages/news-content.blade.php
+++ earth/resources/views/front/pages/news-content.blade.php
@@ -496,7 +496,7 @@ $current_url = $protocol . $host . $uri;
                                                                     <a href="?=url('sign-in/' . Helper::encoded($current_url))?>" class="video-link-without-signin"><i class="fa fa-play-circle-o"></i></a>
                                                                 ?php }?> -->
                                                                 <!-- <a href="https://www.youtube.com/watch?v=?=$other_articles_in_this_series_row->videoId?>" class="video-link"><i class="fa fa-play-circle-o"></i></a> -->
-                                                                <a href="<?=url('category/' . $other_articles_in_this_series_row->parent_category_slug. '/' . $other_articles_in_this_series_row->sub_category_slug)?>"><i class="fa fa-play-circle-o"></i></a>
+                                                                <a href="<?=url('category/' . $other_articles_in_this_series_row->parent_category_slug. '/' . $other_articles_in_this_series_row->sub_category_slug)?>" class="video-link"><i class="fa fa-play-circle-o"></i></a>
                                                             </div>
                                                         <?php } ?>
                                                         <div class="hover-box">
