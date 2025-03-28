acc26a1ec9915f270e95378676843660edd566bd
surajit bera <surajit.b@keylines.net>
2025-03-28 11:59:59 +0530

    work

--- earth/resources/views/front/pages/communities.blade.php
+++ earth/resources/views/front/pages/communities.blade.php
@@ -11,33 +11,39 @@
             <div class="col-md-12 col-sm-12 content-blocker">
                 <!-- block content -->
                 <div class="block-content">
-                    <div class="article-box article-box">                            
-                        <div class="titleto-box2">
-                            <h1>EaRTh COMMUNITIES</h1>  
-                            {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
-                            {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
-                            <p class="black">Currently, we are working with the communities below.</p> --}}
-                            {{-- <div class="titleto-inner">
-                                <ul>
-                                    <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
-                                    
-                                </ul>
-                            </div>
-                            <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
-                            <p><strong class="black">EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
-                            <p><strong class="black">EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
-                            <h3 class="box_heading">But that’s not all EaRTh is.</h3>
-                            <p>Through <strong><i>EaRTh Communities</i></strong>, we help members of Communities (whether formed based on geography or like-minded collaboration) to:</p>
-                            <div class="titleto-inner">
-                                <ul>
-                                    <li><strong class="black"><i>weave</i></strong> with each other, by securely connecting to share ideas, experiences, and knowledge</li>
-                                    <li><strong class="black"><i>thrive</i></strong> together, by co-creating and co-implementing ecology-rooted livelihoods and innovations</li>
-                                </ul>
-                            </div>
-                        </div>    
+                    <div class="article-box">  
+                        <div class="article-post">
+                            <div class="post-content">
+                                <div class="titleto-box2">
+                                    <h1>EaRTh COMMUNITIES</h1>  
+                                    {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
+                                    {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
+                                    <p class="black">Currently, we are working with the communities below.</p> --}}
+                                    {{-- <div class="titleto-inner">
+                                        <ul>
+                                            <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
+                                            
+                                        </ul>
+                                    </div>
+                                    <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
+                                    <p><strong class="black">EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
+                                    <p><strong class="black">EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
+                                    <h3 class="box_heading">But that’s not all EaRTh is.</h3>
+                                    <p>Through <strong><i>EaRTh Communities</i></strong>, we help members of Communities (whether formed based on geography or like-minded collaboration) to:</p>
+                                    <div class="titleto-inner">
+                                        <ul>
+                                            <li><strong class="black"><i>weave</i></strong> with each other, by securely connecting to share ideas, experiences, and knowledge</li>
+                                            <li><strong class="black"><i>thrive</i></strong> together, by co-creating and co-implementing ecology-rooted livelihoods and innovations</li>
+                                        </ul>
+                                    </div>
+                                </div>   
+                            </div>   
+                        </div>
+                                               
+                         
                         <div class="weave_communities">
                             <div class="titleto-inner">
                                 <h2>EaRTh WEAVE COMMUNITIES</h2>
