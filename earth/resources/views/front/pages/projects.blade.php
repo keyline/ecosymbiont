ef2d62be1de89ee956e05d66f49ecdfe184fbb38
surajit bera <surajit.b@keylines.net>
2025-03-28 12:08:53 +0530

    work

--- earth/resources/views/front/pages/projects.blade.php
+++ earth/resources/views/front/pages/projects.blade.php
@@ -11,39 +11,43 @@
             <div class="col-md-12 col-sm-12 content-blocker">
                 <!-- block content -->
                 <div class="block-content">
-                    <div class="article-box">                            
-                        <div class="titleto-box2">
-                            <h1>EaRTh PROJECTS</h1>  
-                            {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
-                            {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
-                            <p class="black">Currently, we are working with the communities below.</p> --}}
-                            {{-- <div class="titleto-inner">
-                                <ul>
-                                    <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
-                                    <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
-                                    
-                                </ul>
+                    <div class="article-box">  
+                        <div class="article-post">
+                            <div class="post-content">
+                                <div class="titleto-box2">
+                                    <h1>EaRTh PROJECTS</h1>  
+                                    {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
+                                    {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
+                                    <p class="black">Currently, we are working with the communities below.</p> --}}
+                                    {{-- <div class="titleto-inner">
+                                        <ul>
+                                            <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
+                                            
+                                        </ul>
+                                    </div>
+                                    <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
+                                    <p><strong class="black">EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
+                                    <p><strong class="black">EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
+                                    <h3 class="box_heading">We have a number of special Projects within EaRTh; anyone is welcome to contribute.</h3>
+                                    <p>We appreciate your patience as we work on providing more details about each of our special <strong class="black"><i>EaRTh Projects.</i></strong></p>
+                                    <div class="titleto-inner">
+                                        <ul>
+                                            <li>Ecoself</li>
+                                            <li>Elders</li>
+                                            <li>FiveBecomings (Pañchabhūmi)</li>
+                                            <li>Interviews</li>
+                                            <li>Webinars</li>
+                                        </ul>
+                                    </div>
+                                </div>     
+                                <div class="thrive_communities">
+                                    <p>If you are interested in submitting one or more Creative-Works to a special <i>EaRTh Project,</i> please <a href="<?=env('REGENERATE_URL')?>contact.php" target="_blank">contact us.</a></p>
+                                </div>  
                             </div>
-                            <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
-                            <p><strong class="black">EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
-                            <p><strong class="black">EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
-                            <h3 class="box_heading">We have a number of special Projects within EaRTh; anyone is welcome to contribute.</h3>
-                            <p>We appreciate your patience as we work on providing more details about each of our special <strong class="black"><i>EaRTh Projects.</i></strong></p>
-                            <div class="titleto-inner">
-                                <ul>
-                                    <li>Ecoself</li>
-                                    <li>Elders</li>
-                                    <li>FiveBecomings (Pañchabhūmi)</li>
-                                    <li>Interviews</li>
-                                    <li>Webinars</li>
-                                </ul>
-                            </div>
-                        </div>     
-                        <div class="thrive_communities">
-                            <p>If you are interested in submitting one or more Creative-Works to a special <i>EaRTh Project,</i> please <a href="<?=env('REGENERATE_URL')?>contact.php" target="_blank">contact us.</a></p>
-                        </div>                  
+                        </div>                                 
                     </div>
                 </div>
                 <!-- End block content -->
