2ea334d0fd22d6e0715f59d27c6f643aefea08e7
Deblina Das <deblina@keylines.net>
2025-03-31 16:28:39 +0530

    co-author short bio updation

--- earth/resources/views/admin/maincontents/news_content/import.blade.php
+++ earth/resources/views/admin/maincontents/news_content/import.blade.php
@@ -368,6 +368,7 @@ function numberToOrdinal($number) {
                                                 <div class="row">
                                                     <label for="pronoun" class="col-md-2 col-lg-4 col-form-label">3I{{$i}}) <?=numberToOrdinal($i)?> co-author’s pronoun</label>
                                                     <div class="col-md-10 col-lg-8">
+                                                        <?php Helper::pr($pronoun); ?>
                                                         @if ($pronoun)
                                                             @foreach ($pronoun as $data)
                                                                 <?php
@@ -382,7 +383,8 @@ function numberToOrdinal($number) {
                                                                 }
                                                                 // $pronoun_checked = '';
                                                                 ?>
-                                                                <input type="radio" name="co_author_pronoun_{{$i}}" value="{{ $data->id }}" <?=$pronoun_checked?>>
+                                                                <!-- <input type="radio" name="co_author_pronoun_{{$i}}" value="{{ $data->id }}" ?=$pronoun_checked?>> -->
+                                                                <input type="radio" name="co_author_pronoun_{{$i}}" value="{{ $data->id }}"  @checked($data->id == $co_author_pronoun[$i - 1]) >
                                                                 <label>{{ $data->name }}</label>
                                                             @endforeach
                                                         @endif                                
