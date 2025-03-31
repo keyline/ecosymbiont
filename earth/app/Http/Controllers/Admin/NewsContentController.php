74b8b0d7244d3dfcf2258b357ad2ec13bad55807
Deblina Das <deblina@keylines.net>
2025-03-31 17:11:17 +0530

    co-author short bio updation

--- earth/app/Http/Controllers/Admin/NewsContentController.php
+++ earth/app/Http/Controllers/Admin/NewsContentController.php
@@ -580,7 +580,7 @@ class NewsContentController extends Controller
                     'other_article_part_doi_no' => $other_article_part_doi_no,
                     'is_published'              => 1,                                                         
                     ];
-                    //  Helper::pr($fields);
+                     Helper::pr($fields);
                     Article::where('id', '=', $id)->update($fields);
                     // return redirect("admin/article/list")->with('success_message', 'Article saved successfully');                
                     return redirect("admin/article/editing-checking")->with('success_message', 'Article saved successfully');                
