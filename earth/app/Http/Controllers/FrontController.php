63501eabedfe575db08b05e6c728267b250a84c5
Deblina Das <deblina@keylines.net>
2025-04-03 10:59:11 +0530

    advance search modification

--- earth/app/Http/Controllers/FrontController.php
+++ earth/app/Http/Controllers/FrontController.php
@@ -476,7 +476,7 @@ class FrontController extends Controller
                                              ->limit(4)
                                              ->get();
             } elseif($search_type == 'Author name'){
-                // DB::enableQueryLog();
+                DB::enableQueryLog();
                 $data['contents'] = NewsContent::select(
                                             'news_contents.id', 
                                             'news_contents.new_title', 
@@ -500,7 +500,8 @@ class FrontController extends Controller
                                         ->where('news_contents.status', 1)
                                         ->where(function ($query) use ($search_keyword) {
                                             $query->where('news_contents.author_name', 'LIKE', '%' . $search_keyword . '%')
-                                                  ->orWhereRaw("JSON_CONTAINS(news_contents.co_author_names, ?)", [json_encode($search_keyword)]);
+                                                //   ->orWhereRaw("JSON_CONTAINS(news_contents.co_author_names, 'LIKE')", '%'. [json_encode($search_keyword)] . '%')
+                                                ->orWhereRaw("JSON_SEARCH(news_contents.co_author_names, 'all', ?)", [$search_keyword]);
                                         })
                                         ->where(function ($query) {
                                             $query->whereNull('news_contents.current_article_no')
@@ -510,7 +511,7 @@ class FrontController extends Controller
                                         ->orderBy('news_contents.created_at', 'DESC')
                                         ->limit(4)
                                         ->get();            
-                                            //   dd(DB::getQueryLog());
+                                              dd(DB::getQueryLog());
             } elseif($search_type == 'Subtitle'){
                 $data['contents']   = NewsContent::select(
                                                         'news_contents.id', 
