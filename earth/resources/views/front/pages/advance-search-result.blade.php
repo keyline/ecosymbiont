e2463b9d1122b617ec85aa9cc0546a6fff76ebe7
Deblina Das <deblina@keylines.net>
2025-04-03 11:26:27 +0530

    advance search modification

--- earth/resources/views/front/pages/advance-search-result.blade.php
+++ earth/resources/views/front/pages/advance-search-result.blade.php
@@ -360,7 +360,7 @@ $current_url = $protocol . $host . $uri;
                 }              
             },
             error: function () {
-                alert('End of search results');
+                alert('End of search results with error');
                 $('#loading').hide();
             }
         });
