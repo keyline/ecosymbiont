5c8011768cf5d0f4b0a3b7adef24234e81783ee7
Deblina Das <deblina@keylines.net>
2025-04-07 20:43:12 +0530

    home control creation

--- earth/resources/views/admin/maincontents/home-control-info.blade.php
+++ earth/resources/views/admin/maincontents/home-control-info.blade.php
@@ -6,7 +6,7 @@ use App\Helpers\Helper;
   <nav>
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="<?=url('admin/dashboard')?>">Home</a></li>
-      <li class="breadcrumb-item"><a href="<?=url('admin/email-logs')?>">Email-logs List</a></li>
+      <li class="breadcrumb-item"><a href="<?=url('admin/home-control')?>">Home Control Panel</a></li>
       <li class="breadcrumb-item active"><?=$page_header?></li>
     </ol>
   </nav>
