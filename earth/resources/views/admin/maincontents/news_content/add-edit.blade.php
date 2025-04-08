369070a37557209516e9d87d6de6523dafb56643
Deblina Das <deblina@keylines.net>
2025-04-08 15:42:35 +0530

    citation insertion

--- earth/resources/views/admin/maincontents/news_content/add-edit.blade.php
+++ earth/resources/views/admin/maincontents/news_content/add-edit.blade.php
@@ -631,8 +631,8 @@ function numberToOrdinal($number) {
                                     <textarea name="long_desc" class="form-control ckeditor" id="long_desc" rows="5"><?= $long_desc ?></textarea>
                                 </div>
                                 <div id="field-repeater">
-                                    <div class="input-group mb-2" data-index="1">
-                                        <input type="text" name="fields[1][value]" class="form-control" placeholder="Enter value">
+                                    <div class="col-md-10 col-lg-8" data-index="1">
+                                        <input type="text" name="fields[1][value]" class="form-control">
                                         <input type="hidden" name="fields[1][id]" value="field_1">
                                         <button class="btn btn-outline-secondary copy-btn" type="button" data-id="field_1">Copy ID</button>
                                         <button class="btn btn-outline-danger remove-field" type="button">-</button>
