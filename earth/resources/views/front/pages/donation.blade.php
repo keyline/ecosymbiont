c6476323279b9822888e60ad80eaf88c1853f35c
surajit bera <surajit.b@keylines.net>
2025-04-17 16:54:24 +0530

    new page add

---
+++ earth/resources/views/front/pages/donation.blade.php
@@ -0,0 +1,131 @@
+<?php
+//  use Illuminate\Support\Facades\Route;
+//  $routeName = Route::current();
+//  $pageName = explode('/', $routeName->uri());
+//  $pageSegment = $pageName[1];
+?>
+<!-- block-wrapper-section ================================================== -->
+<section class="block-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 content-blocker">
+                <!-- block content -->
+                <div class="block-content">
+                    <div class="article-box">  
+                        <div class="article-post">
+                            <div class="post-content">
+                                <div class="titleto-box2">
+                                    <h1>EaRTh COMMUNITIES</h1>  
+                                </div>
+                                <div class="news-post article-post">
+                                    <div class="post-content">
+                                        <p><strong class="black">EaRTh</strong> is a custom-built, professionally edited knowledge-sharing and community-building platform for grassroots changemakers, innovators, and knowledge-holders (creators) all across the world.</p>
+                                        <p><strong class="black">EaRTh</strong> honors all (digitizable) modes of knowledge-sharing — through written narratives, images, and videos — of fiction and non-fiction, work and creative expression (visual art, music, performance), ideas and implementations, and personal stories and professional interviews.</p>
+                                        <h3 class="box_heading">But that’s not all EaRTh is.</h3>
+                                        <p>Through <strong><i>EaRTh Communities</i></strong>, we help members of Communities (whether formed based on geography or like-minded collaboration) to:</p>
+                                        <div class="titleto-inner">
+                                            <ul>
+                                                <li><strong class="black"><i>weave</i></strong> with each other, by securely connecting to share ideas, experiences, and knowledge</li>
+                                                <li><strong class="black"><i>thrive</i></strong> together, by co-creating and co-implementing ecology-rooted livelihoods and innovations</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                    {{-- <p class="black">As part of its <a target="_blank" href="?=env('REGENERATE_URL')?>" style="color: #d09c1c;">Ecosymbionts Regenerate</a> initiative, the Śramani Institute provides several communities (whether formed through geography or like-minded collaboration) a <strong><em>Virtual Community Center</em></strong> dedicated to the specific needs of each community.</p> --}}
+                                    {{-- <p class="black">We appreciate your patience as we develop this feature.</p>
+                                    <p class="black">Currently, we are working with the communities below.</p> --}}
+                                    {{-- <div class="titleto-inner">
+                                        <ul>
+                                            <li><a target="_blank" href="?=url('communities/ACTChangemakers')?>" style="color: #d09c1c;"><em>ACT Changemakers Web</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/EthosFellows')?>" style="color: #d09c1c;"><em>Ethos Fellows Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/SchumacherWild')?>" style="color: #d09c1c;"><em>Schumacher Wild Network</em></a> (global)</li>
+                                            <li><a target="_blank" href="?=url('communities/WestOakland')?>" style="color: #d09c1c;"><em>West Oakland Living Heritage Center</em></a> (USA)</li>
+                                            
+                                        </ul>
+                                    </div>
+                                    <p class="black">If your community is interested in having a <em>Virtual Community Center</em>, please <a href="?=env('REGENERATE_URL')?>contact.php" style="color: #d09c1c;">contact us.</a></p> --}}
+                            </div>   
+                        </div>
+                                               
+                        <div class="article-post">
+                            <div class="post-content">
+                                <div class="weave_communities">
+                                    <div class="titleto-inner">
+                                        <h2>EaRTh WEAVE COMMUNITIES</h2>
+                                    </div>
+                                    <div class="row" style="margin: 20px 0;">
+                                        <div class="col-md-2">
+                                            <img src="<?=env('UPLOADS_URL').'EaRTh-Weave-Communities-Text-Logo.jpg'?>" alt="" style="width: 100%; max-width: 160px; margin-bottom: 20px;">
+                                        </div>
+                                        <div class="col-md-10">
+                                            <div class="titleto-inner">
+                                                <ul>
+                                                    <li>a Community-composed public-facing page on the EaRTh platform</li>
+                                                    <li>tailored private pages accessible upon secure login through a private portal</li>
+                                                    <li>a feature enabling private interactive communication between members without needing to exchange contact information until trust is established</li>
+                                                    <li>searchable and customizable knowledge- and information-sharing</li>
+                                                </ul>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    <h3>Our Current EaRTh Weave Communities</h3>
+                                    <p>We appreciate your patience as we work with each Community to develop their public profile.</p>
+                                    <div class="titleto-inner">
+                                        <ul>
+                                            <li>
+                                                <a target="_blank" href="<?=url('communities/ACTChangemakers')?>" style="text-decoration: underline;"><i>ACT Changemakers Web</i></a> (global)
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="<?=url('communities/EthosFellows')?>" style="text-decoration: underline;"><i>Ethos Fellows Network</i></a> (global)
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="<?=url('communities/SchumacherWild')?>" style="text-decoration: underline;"><i>Schumacher Wild Network</i></a> (global)
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>  
+                                <div class="thrive_communities">
+                                    <div class="titleto-inner">
+                                        <h2>EaRTh THRIVE COMMUNITIES</h2>
+                                    </div>
+                                    <div class="row" style="margin: 20px 0;">
+                                        <div class="col-md-2">
+                                            <img src="<?=env('UPLOADS_URL').'EaRTh-Thrive-Communities-Text-Logo.jpg'?>" alt="" style="width: 100%; max-width: 160px; margin-bottom: 20px;">
+                                        </div>
+                                        <div class="col-md-10">
+                                            <div class="titleto-inner">
+                                                <ul>
+                                                    <li>all features available to <i>EaRTh Weave Communities</i></li>
+                                                    <li>living wages for Community members submitting creative-works published on EaRTh</li>
+                                                    <li>funding for ecoweb-rooted livelihoods and innovations collaboratively generated and implemented by Community members, with sharing of knowledge within the Community</li>
+                                                    <li>access to expertise of EaRTh creators outside of the Community</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h3>Our Current EaRTh Thrive Communities</h3>
+                                    <p>We appreciate your patience as we work with each Community to develop their public profile.</p>
+                                        <div class="titleto-inner">
+                                            <ul>
+                                                <li>
+                                                    <i>Hella Love</i> (USA)
+                                                </li>
+                                                <li>
+                                                    <a target="_blank" href="<?=url('communities/WestOakland')?>" style="text-decoration: underline;"><i>West Oakland Living Heritage Center</i></a> (USA)
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    <p>If you are interested in becoming an <i>EaRTh Community</i>, please <a href="<?=env('REGENERATE_URL')?>contact.php" target="_blank">contact us.</a></p>
+                                </div>  
+                            </div>
+                        </div>
+                                        
+                    </div>
+                </div>
+                <!-- End block content -->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End block-wrapper-section -->
