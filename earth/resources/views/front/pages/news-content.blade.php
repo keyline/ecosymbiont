41db4ebb621e6cec1dc962ef56beab50b162d14d
Deblina Das <deblina@keylines.net>
2025-04-18 14:34:08 +0530

    feedback 18

--- earth/resources/views/front/pages/news-content.blade.php
+++ earth/resources/views/front/pages/news-content.blade.php
@@ -627,7 +627,7 @@ $current_url = $protocol . $host . $uri;
                 ?></p>  
         </div>
         <div class="cite_button">
-            <button class="btn btn-primary" onclick="copyText()"><i class="fa fa-copy"></i> Copy</button>                                  
+            <!-- <button class="btn btn-primary" onclick="copyText()"><i class="fa fa-copy"></i> Copy</button>                                   -->
             <button id="closePopup">Close</button>
         </div>                                    
         <h3 id="copyMessage" class="text-success">Copied successfully!</h3>
