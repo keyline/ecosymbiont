b93327604d7267bd00f3315d002d7cc6a1835c8a
subhomoy samanta <subhomoy@keylines.net>
2025-04-22 19:09:21 +0530

    donation module implement

--- earth/app/Http/Controllers/DonationController.php
+++ earth/app/Http/Controllers/DonationController.php
@@ -35,6 +35,7 @@ use App\Models\UserProfile;
 use App\Models\UserClassification;
 use App\Models\Community;
 use App\Models\Project;
+use App\Models\Donation;
 use Auth;
 use Session;
 use Helper;
@@ -47,12 +48,63 @@ date_default_timezone_set("Asia/Calcutta");
 
 class DonationController extends Controller
 {
-    public function donation()
+    public function donation(Request $request)
     {
         $data['countries']              = Country::select('id', 'name')->where('status', 1)->orderBy('name', 'ASC')->get();
         $title                          = 'Donation';
         $page_name                      = 'donation';
-        $data['search_keyword']         = '';
+        if ($request->isMethod('post')) {
+            $postData = $request->all();
+            $rules = [
+                'first_name'                    => 'required',
+                'last_name'                     => 'required',
+                'email'                         => 'required',
+                'address'                       => 'required',
+                'country'                       => 'required',
+                'payment_mode'                  => 'required',
+                'payable_amount'                => 'required',
+            ];
+            if ($this->validate($request, $rules)) {
+                $country            = $request->country;
+                $getCountry         = Country::select('id', 'name', 'ISO')->where('id', $country)->first();
+                $getLastDonation    = Donation::orderBy('id', 'DESC')->first();
+                // SRM-US-web2025-00123
+                if($getLastDonation){
+                    $sl_no              = $getLastDonation->sl_no;
+                    $next_sl_no         = $sl_no + 1;
+                    $next_sl_no_string  = str_pad($next_sl_no, 5, 0, STR_PAD_LEFT);
+                    $donation_number    = 'SRM-'.(($getCountry)?$getCountry->ISO:'').'-web'.date('Y').'-'.$next_sl_no_string;
+                } else {
+                    $next_sl_no         = 1;
+                    $next_sl_no_string  = str_pad($next_sl_no, 5, 0, STR_PAD_LEFT);
+                    $donation_number    = 'SRM-'.(($getCountry)?$getCountry->ISO:'').'-web'.date('Y').'-'.$next_sl_no_string;
+                }
+                $fields = [
+                    'sl_no'                                 => $postData['name'],
+                    'donation_number'                       => $postData['name'],
+                    'first_name'                            => $postData['first_name'],
+                    'last_name'                             => $postData['last_name'],
+                    'email'                                 => $postData['email'],
+                    'address'                               => $postData['address'],
+                    'country'                               => $postData['country'],
+                    'payment_mode'                          => $postData['payment_mode'],
+                    'payable_amount'                        => $postData['payable_amount'],
+                ];
+                Helper::pr($fields);
+                $donation_id = Donation::insertGetId($fields);
+                return redirect(url('donation-preview/' . Helper::encoded($donation_id)))->with('success_message', 'Basic info inserted for donation');
+            } else {
+                return redirect()->back()->with('error_message', 'All fields required');
+            }
+        }
+        echo $this->front_before_login_layout($title, $page_name, $data);
+    }
+    public function donationPreview($donation_id)
+    {
+        $donation_id                    = Helper::decoded($donation_id);
+        $data['donation']               = Donation::where('id', $donation_id)->first();
+        $title                          = 'Donation Preview';
+        $page_name                      = 'donation-preview';
         echo $this->front_before_login_layout($title, $page_name, $data);
     }
     public function thankyou()
@@ -60,7 +112,6 @@ class DonationController extends Controller
         $data = [];
         $title                          = 'Thankyou';
         $page_name                      = 'thankyou';
-        $data['search_keyword']         = '';
         echo $this->front_before_login_layout($title, $page_name, $data);
     }
     public function donationreceipt()
@@ -68,7 +119,6 @@ class DonationController extends Controller
         $data = [];
         $title                          = 'Donationreceipt';
         $page_name                      = 'donationreceipt';
-        $data['search_keyword']         = '';
         echo $this->front_before_login_layout($title, $page_name, $data);
     }
 }
