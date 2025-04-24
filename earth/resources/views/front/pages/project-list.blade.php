df6e866f299ba4df9cfe2a3c10fe13601dd9e3cc
Deblina Das <deblina@keylines.net>
2025-04-24 12:10:30 +0530

    feedback 24

--- earth/resources/views/front/pages/project-list.blade.php
+++ earth/resources/views/front/pages/project-list.blade.php
@@ -75,7 +75,7 @@ $current_url = $protocol . $host . $uri;
                                                         <li><i class="fa fa-clock-o"></i><?=date_format(date_create($rowContent->created_at), "d M Y")?></li>
                                                         <li><i class="fa fa-user"></i>by <a href="javascript:void(0);"><?= $rowContent->for_publication_name ?? $rowContent->author_name?></a></li>
                                                         <?php if($rowContent->projects_name != ''){ ?>
-                                                        <li><a class="btn project-btn" href="<?= url('project/' .$rowContent->projects_name)?>"><i class="fa fa-users"></i> <?=$rowContent->projects_name?></a></li>
+                                                        <li>PROJECTS:<a class="btn project-btn" href="<?= url('project/' .$rowContent->projects_name)?>"><i class="fa fa-users"></i> <?=$rowContent->projects_name?></a></li>
                                                         <?php } ?>
                                                         <!-- <li><a href="#"><i class="fa fa-comments-o"></i><span>23</span></a></li>
                                                         <li><i class="fa fa-eye"></i>872</li> -->
