ea95a01d0dec96e7a0a06307062d52473af6ed7e
surajit bera <surajit.b@keylines.net>
2025-04-25 11:47:37 +0530

    pdf change

--- earth/resources/views/front/pages/donationreceipt.blade.php
+++ earth/resources/views/front/pages/donationreceipt.blade.php
@@ -137,7 +137,7 @@ $getcountry                 = Country::find($donation->country);
             border: none;
             border-top: 1px solid #DAA600;
             width: 82%;
-            margin: 1px 0;
+            margin: 0px 0 0px auto;
         }
 
         .text-right a {
