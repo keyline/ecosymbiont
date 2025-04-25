ebef0a62455e4d9d13a81b2a0dec8424d2c4ba2a
surajit bera <surajit.b@keylines.net>
2025-04-25 13:33:50 +0530

    pdf change

--- earth/resources/views/front/pages/donationreceipt.blade.php
+++ earth/resources/views/front/pages/donationreceipt.blade.php
@@ -160,7 +160,7 @@ $getcountry                 = Country::find($donation->country);
                     <div class="receipt-card">
                         <div class="receipt-header-top">
                             <div class="logo-left">
-                                <img style="width: 130px; height: 130px;" src="data:image/svg+xml;base64,<?php echo base64_encode(file_get_contents(base_path('public/uploads/pdf-logo.png'))); ?>">
+                                <img style="width: 130px; height: 130px;" src="data:image/svg+xml;base64,<?php echo base64_encode(file_get_contents(base_path('public/uploads/pdf-logo.jpg'))); ?>">
                             </div>
                             <div class="text-right">
                                 <strong class="green">Realizing the interconnected wellbeing<br>of humans and ecologies</strong><br>
