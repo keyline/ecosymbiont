628354f00065ddee7e32f0cc67d64c260ac360a3
Deblina Das <deblina@keylines.net>
2025-04-25 19:01:11 +0530

    feedback 25

--- earth/resources/views/front/pages/user/submit-new-article.blade.php
+++ earth/resources/views/front/pages/user/submit-new-article.blade.php
@@ -265,6 +265,7 @@ use Illuminate\Support\Facades\DB;
                                     <input type="radio" id="co_authors_2" name="co_authors" value="2" required @checked(old('co_authors', $co_authors ?? '') == '2')>
                                     <label for="2">2</label>
                                 </div>
+                                <div id="co_authors-error" class="error-msg" style="display: none;"></div>
                             </div>
                             <div id="co_authors_position" style="display: none;border: 1px solid #000; padding: 10px; border-radius: 7px; margin-bottom: 20px;">
                                 <div class="row mb-3">
@@ -453,6 +454,7 @@ use Illuminate\Support\Facades\DB;
                                     <input type="radio" id="no" name="orginal_work" value="No" required @checked(old('orginal_work', $orginal_work ?? '') == 'No') onclick="showModal()">
                                     <label for="no">No</label>
                                 </div>
+                                <div id="orginal_work-error" class="error-msg" style="display: none;"></div>
                             </div>
                             <div class="row mb-3">
                                 <label for="copyright" class="col-md-2 col-lg-4 col-form-label blue-text">9) Do you own the copyright and licensing rights to all components of your Creative-Work?
@@ -463,6 +465,7 @@ use Illuminate\Support\Facades\DB;
                                     <input type="radio" id="no" name="copyright" value="No" required @checked(old('copyright', $copyright ?? '') == 'No') onclick="showModal()">
                                     <label for="no">No</label>
                                 </div>
+                                <div id="copyright-error" class="error-msg" style="display: none;"></div>
                             </div>                              
                             <!-- Modal -->
                             <!-- <div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
@@ -1120,12 +1123,25 @@ use Illuminate\Support\Facades\DB;
     });
     </script> -->
     <script>
+       $('#submitBtn').on('click', function (e) {
+        let isValid = true;
+
         $('input[type="radio"][required]').each(function () {
-    const name = $(this).attr('name');
-    if ($('input[name="' + name + '"]:checked').length === 0) {
-        $('input[name="' + name + '"]').addClass('required-error');
-    }
-});
+            const name = $(this).attr('name');
+            if ($('input[name="' + name + '"]:checked').length === 0) {
+                $('#'+name+'-error').text('This field is required.').show();
+                $('input[name="' + name + '"]').first().focus();
+                isValid = false;
+                return false; // break loop on first error
+            } else {
+                $('#'+name+'-error').hide();
+            }
+        });
+
+        if (!isValid) {
+            e.preventDefault();
+        }
+    });
     </script>
     <script>
         $(document).ready(function () {
