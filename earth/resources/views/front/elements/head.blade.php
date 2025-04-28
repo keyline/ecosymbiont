f839c4628216a325cb6d80089b64238e138b2395
Deblina Das <deblina@keylines.net>
2025-04-28 18:57:58 +0530

    validation checking

--- earth/resources/views/front/elements/head.blade.php
+++ earth/resources/views/front/elements/head.blade.php
@@ -47,3 +47,12 @@ $fullUrl = $protocol . $host . $requestUri;
       // <meta name="keywords" content="home">
     }
     ?>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZF0FJHP0P2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZF0FJHP0P2');
+</script>
