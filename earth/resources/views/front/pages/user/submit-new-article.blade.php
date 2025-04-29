030799231c74a015d09fe635460c0fde09c03b2f
Deblina Das <deblina@keylines.net>
2025-04-29 17:17:03 +0530

    validation checking and citation modification

--- earth/resources/views/front/pages/user/submit-new-article.blade.php
+++ earth/resources/views/front/pages/user/submit-new-article.blade.php
@@ -1676,8 +1676,7 @@ $(document).ready(function () {
         
         function validateFileType(input, errorElementId) {
             const file = input.files[0];
-            const inputName = input.getAttribute('name');
-            alert(inputName);
+            const inputName = input.getAttribute('name');            
             const errorElement = document.getElementById(errorElementId);
             
             // Define allowed types
