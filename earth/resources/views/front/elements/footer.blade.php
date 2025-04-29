8b4aa9eac67dcbf44afba7ffc788477029a23042
surajit bera <surajit.b@keylines.net>
2025-04-29 16:37:32 +0530

    popup show

--- earth/resources/views/front/elements/footer.blade.php
+++ earth/resources/views/front/elements/footer.blade.php
@@ -146,7 +146,7 @@ $projects  = Project::select('id', 'name')->where('status', '=', 1)->orderBy('na
         <p><u>But we can’t do this without your financial help.</u></p>
         <p>While EaRTh is free to use, we (a small nonprofit organization) need to pay the editorial staff, designers, software developers, and others who ensure that EaRTh runs smoothly, so we can serve those who need most to be heard.</p>
         {{-- <p>Join the fight today,<br><span class="highlight">your gift will be matched $2:$1!</span></p> --}}
-        <a href="<?=url('donation')?>" class="btn btn-yellow">Donate to EaRTh</a><br>
+        <a href="<?=url('donation')?>" target="_blank" class="btn btn-yellow">Donate to EaRTh</a><br>
         <a href="javascript:void(0);" class="continue-link" data-dismiss="modal">Continue to website →</a>
       </div>
     </div>
