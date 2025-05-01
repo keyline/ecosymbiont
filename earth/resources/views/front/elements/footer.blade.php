daefb4c3d1ebad9732fe93882666b41cf0f2d411
Deblina Das <deblina@keylines.net>
2025-05-01 16:42:49 +0530

    profile validation

--- earth/resources/views/front/elements/footer.blade.php
+++ earth/resources/views/front/elements/footer.blade.php
@@ -215,6 +215,7 @@ $projects  = Project::select('id', 'name')->where('status', '=', 1)->orderBy('na
 <a href="<?=url('donation')?>" target="_blank" class="donate-button">Donate to EaRTH</a>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script>
     $(window).on('load', function () {
     $('#popupModal').modal('show');
