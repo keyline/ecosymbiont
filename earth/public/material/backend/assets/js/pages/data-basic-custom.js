4032231e58616e25045047543176ee2bd08bdf57
Deblina Das <deblina@keylines.net>
2025-05-12 17:52:22 +0530

    multiple selection delete

--- earth/public/material/backend/assets/js/pages/data-basic-custom.js
+++ earth/public/material/backend/assets/js/pages/data-basic-custom.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
             columnDefs: [
         {
             orderable: false,
-            targets: no-sort // First column (checkboxes)
+            targets: 'no-sort' // First column (checkboxes)
         }
     ]
         });
