2736f17e3957b1d38b91d92ee8396138502c5cd8
Deblina Das <deblina@keylines.net>
2025-05-27 16:35:38 +0530

    category load

--- earth/resources/views/front/pages/advance-search-result.blade.php
+++ earth/resources/views/front/pages/advance-search-result.blade.php
@@ -167,10 +167,10 @@ $current_url = $protocol . $host . $uri;
                                                     <i class="fa fa-user"></i> by 
                                                     <a href="javascript:void(0);">${content.for_publication_name ?? content.author_name}</a>
                                                 </li> 
-                                                <?php if (!empty($rowContent->projects_name)) { ?>
+                                                <?php if (!empty($content->projects_name)) { ?>
                                                 <li>
-                                                    <a class="btn project-btn" href="<?= url('project/' . $rowContent->projects_name) ?>">
-                                                        <i class="fa fa-users"></i> <?= $rowContent->projects_name ?>
+                                                    <a class="btn project-btn" href="<?= url('project/' . $content->projects_name) ?>">
+                                                        <i class="fa fa-users"></i> <?= $content->projects_name ?>
                                                     </a>
                                                 </li>
                                                 <?php } ?>                                              
