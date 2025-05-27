eda8b2088bc8bfdc7ca665ad00a629d6e0a0d78a
Deblina Das <deblina@keylines.net>
2025-05-27 14:51:53 +0530

    category load

--- earth/resources/views/front/pages/project-list.blade.php
+++ earth/resources/views/front/pages/project-list.blade.php
@@ -130,7 +130,7 @@ $current_url = $protocol . $host . $uri;
     $('#load_more_btn').on('click', function () {
         $('#loading').show();
         $.ajax({
-            url: '<?= url('category_load/' .$slug) ?>',
+            url: '<?= url('project_load/' .$slug) ?>',
             type: 'POST',
             data: {
                 offset: offset,
