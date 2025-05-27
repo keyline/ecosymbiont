5841623a08ae0050dc7cefb9786bee893062f68c
Deblina Das <deblina@keylines.net>
2025-05-27 14:15:11 +0530

    category load

--- earth/resources/views/front/pages/subcategory.blade.php
+++ earth/resources/views/front/pages/subcategory.blade.php
@@ -44,83 +44,81 @@ $current_url = $protocol . $host . $uri;
                                     </div>
                                 </div>
                             <?php } else { 
-                                foreach ($contents as $rowContent) { ?>
-                                    <?php
-                                    $is_series                  = $rowContent->is_series;
-                                    $series_article_no          = $rowContent->series_article_no;
-                                    $current_article_no         = $rowContent->current_article_no;
-                                    $other_article_part_doi_no      = explode(",", $rowContent->other_article_part_doi_no);
-                                    if($is_series == 'Yes'){
-                                        if($current_article_no == 1){
-                                            $isShow = true;
-                                        } else {
-                                            $isShow = false;
-                                        }
-                                    } else {
-                                        $isShow = true;
-                                    }
-                                    if($isShow){
-                                    ?>
-                                        <div class="news-post article-post">
-                                            <div class="row">
-                                                <div class="col-sm-5">
-                                                    <!-- <div class="post-gallery">
-                                                        <img src="<?= env('UPLOADS_URL') . 'newcontent/' . $rowContent->cover_image ?>" alt="<?= $rowContent->new_title ?>">
-                                                    </div> -->
-                                                    <?php if($rowContent->media == 'image'){?>
-                                                        <div class="post-gallery">
-                                                            <img src="<?=env('UPLOADS_URL').'newcontent/'.$rowContent->cover_image?>" alt="<?=$rowContent->new_title?>">
-                                                            <span class="image-caption" style="color:skyblue;"><?=$rowContent->cover_image_caption?></span>
-                                                        </div>
-                                                    <?php } else {?>
-                                                        <div class="post-gallery video-post">
-                                                            <img alt="" src="https://img.youtube.com/vi/<?=$rowContent->videoId?>/hqdefault.jpg">
-                                                            <!-- ?php if(session('is_user_login')){?> -->
-                                                                <!-- <a href="https://www.youtube.com/watch?v=?=$rowContent->videoId?>" class="video-link"><i class="fa fa-play-circle-o"></i></a> -->
-                                                                <a href="<?= url('content/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug . '/' . $rowContent->slug) ?>" class="video-link"><i class="fa fa-play-circle-o"></i></a>
-                                                            <!-- ?php } else {?> -->
-                                                                <!-- <a href="?=url('sign-in/' . Helper::encoded($current_url))?>" class="video-link-without-signin"><i class="fa fa-play-circle-o"></i></a> -->
-                                                            <!-- ?php }?> -->
-                                                        </div>
-                                                    <?php } ?>
+                            foreach ($contents as $rowContent) { ?>
+                            <?php
+                            $is_series                  = $rowContent->is_series;
+                            $series_article_no          = $rowContent->series_article_no;
+                            $current_article_no         = $rowContent->current_article_no;
+                            $other_article_part_doi_no      = explode(",", $rowContent->other_article_part_doi_no);
+                            if($is_series == 'Yes'){
+                                if($current_article_no == 1){
+                                    $isShow = true;
+                                } else {
+                                    $isShow = false;
+                                }
+                            } else {
+                                $isShow = true;
+                            }
+                            if($isShow){
+                            ?>
+                                <div class="news-post article-post">
+                                    <div class="row">
+                                        <div class="col-sm-5">
+                                            <!-- <div class="post-gallery">
+                                                <img src="<?= env('UPLOADS_URL') . 'newcontent/' . $rowContent->cover_image ?>" alt="<?= $rowContent->new_title ?>">
+                                            </div> -->
+                                            <?php if($rowContent->media == 'image'){?>
+                                                <div class="post-gallery">
+                                                    <img src="<?=env('UPLOADS_URL').'newcontent/'.$rowContent->cover_image?>" alt="<?=$rowContent->new_title?>">
+                                                    <span class="image-caption" style="color:skyblue;"><?=$rowContent->cover_image_caption?></span>
                                                 </div>
-                                                <div class="col-sm-7">
-                                                    <div class="post-content">
-                                                        <a href="<?= url('category/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug) ?>"><?= $rowContent->sub_category_name ?></a>
-                                                        <h2><a href="<?= url('content/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug . '/' . $rowContent->slug) ?>"><?= $rowContent->new_title ?></a></h2>
-                                                        <ul class="post-tags">
-                                                            <li><i class="fa fa-clock-o"></i><?= date_format(date_create($rowContent->created_at), "d M Y") ?></li>
-                                                            <li><i class="fa fa-user"></i>by <a href="javascript:void(0);"><?= $rowContent->for_publication_name ?? $rowContent->author_name ?></a></li>
-                                                            <?php if($rowContent->projects_name != ''){ ?>
-                                                        <li><a class="btn project-btn" href="<?= url('project/' .$rowContent->projects_name)?>"><i class="fa fa-users"></i> <?=$rowContent->projects_name?></a></li>
-                                                        <?php } ?>
-                                                        </ul>
-                                                        <p><?= $rowContent->sub_title ?></p>
-                                                    </div>
+                                            <?php } else {?>
+                                                <div class="post-gallery video-post">
+                                                    <img alt="" src="https://img.youtube.com/vi/<?=$rowContent->videoId?>/hqdefault.jpg">
+                                                    <!-- ?php if(session('is_user_login')){?> -->
+                                                        <!-- <a href="https://www.youtube.com/watch?v=?=$rowContent->videoId?>" class="video-link"><i class="fa fa-play-circle-o"></i></a> -->
+                                                        <a href="<?= url('content/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug . '/' . $rowContent->slug) ?>" class="video-link"><i class="fa fa-play-circle-o"></i></a>
+                                                    <!-- ?php } else {?> -->
+                                                        <!-- <a href="?=url('sign-in/' . Helper::encoded($current_url))?>" class="video-link-without-signin"><i class="fa fa-play-circle-o"></i></a> -->
+                                                    <!-- ?php }?> -->
                                                 </div>
+                                            <?php } ?>
+                                        </div>
+                                        <div class="col-sm-7">
+                                            <div class="post-content">
+                                                <a href="<?= url('category/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug) ?>"><?= $rowContent->sub_category_name ?></a>
+                                                <h2><a href="<?= url('content/' . $rowContent->parent_category_slug. '/' . $rowContent->sub_category_slug . '/' . $rowContent->slug) ?>"><?= $rowContent->new_title ?></a></h2>
+                                                <ul class="post-tags">
+                                                    <li><i class="fa fa-clock-o"></i><?= date_format(date_create($rowContent->created_at), "d M Y") ?></li>
+                                                    <li><i class="fa fa-user"></i>by <a href="javascript:void(0);"><?= $rowContent->for_publication_name ?? $rowContent->author_name ?></a></li>
+                                                    <?php if($rowContent->projects_name != ''){ ?>
+                                                <li><a class="btn project-btn" href="<?= url('project/' .$rowContent->projects_name)?>"><i class="fa fa-users"></i> <?=$rowContent->projects_name?></a></li>
+                                                <?php } ?>
+                                                </ul>
+                                                <p><?= $rowContent->sub_title ?></p>
                                             </div>
                                         </div>
-                                    <?php }?>
-                                <?php }  
-                            } ?>    
-                            <?php if(count($contents) >= 4) { ?>
-                            <button id="load_more_btn" style="background-color: #d09c1c;border: #d09c1c;display: flex;margin: 0 auto;" class="btn btn-primary">Load More</button>
-                            <?php }?>
-                            <div id="loading" style="display: none;text-align: center;">
-                                <svg version="1.1" id="L5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                                    <circle fill="#ed1c24" stroke="none" cx="6" cy="50" r="6">
-                                        <animateTransform attributeName="transform" dur="1s" type="translate" values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1" />
-                                    </circle>
-                                    <circle fill="#ed1c24" stroke="none" cx="30" cy="50" r="6">
-                                        <animateTransform attributeName="transform" dur="1s" type="translate" values="0 10 ; 0 -10; 0 10" repeatCount="indefinite" begin="0.2" />
-                                    </circle>
-                                    <circle fill="#ed1c24" stroke="none" cx="54" cy="50" r="6">
-                                        <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5" repeatCount="indefinite" begin="0.3" />
-                                    </circle>
-                                </svg>
-                            </div>   
-                        </div>                
+                                    </div>
+                                </div>
+                            <?php } } } ?> 
+                        </div>   
+                        <?php if(count($contents) >= 4) { ?>
+                        <button id="load_more_btn" style="background-color: #d09c1c;border: #d09c1c;display: flex;margin: 0 auto;" class="btn btn-primary">Load More</button>
+                        <?php }?>
+                        <div id="loading" style="display: none;text-align: center;">
+                            <svg version="1.1" id="L5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                                <circle fill="#ed1c24" stroke="none" cx="6" cy="50" r="6">
+                                    <animateTransform attributeName="transform" dur="1s" type="translate" values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1" />
+                                </circle>
+                                <circle fill="#ed1c24" stroke="none" cx="30" cy="50" r="6">
+                                    <animateTransform attributeName="transform" dur="1s" type="translate" values="0 10 ; 0 -10; 0 10" repeatCount="indefinite" begin="0.2" />
+                                </circle>
+                                <circle fill="#ed1c24" stroke="none" cx="54" cy="50" r="6">
+                                    <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5" repeatCount="indefinite" begin="0.3" />
+                                </circle>
+                            </svg>
+                        </div>                                        
                         <!-- End article box -->
                     </div>
                     <!-- End block content -->
