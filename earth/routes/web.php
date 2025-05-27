0a96ed6dae4c2c8b398ef608de5ae0ea23ae2562
Deblina Das <deblina@keylines.net>
2025-05-27 13:56:17 +0530

    category load

--- earth/routes/web.php
+++ earth/routes/web.php
@@ -29,6 +29,7 @@ use App\Http\Controllers\PayPalController;
         Route::match(['get', 'post'], '/category/{id}', 'App\Http\Controllers\FrontController@category');
         Route::match(['get', 'post'], '/category_load/{id}', 'App\Http\Controllers\FrontController@category_load');
         Route::match(['get', 'post'], '/category/{categoryname}/{id}', 'App\Http\Controllers\FrontController@subcategory');
+        Route::match(['get', 'post'], '/category_load/{categoryname}/{id}', 'App\Http\Controllers\FrontController@subcategory_load');
         Route::match(['get', 'post'], '/project/{id}', 'App\Http\Controllers\FrontController@project');
         Route::match(['get', 'post'], '/content/{categoryname}/{subcategoryname}/{id}', 'App\Http\Controllers\FrontController@newsContent');
         Route::match(['get', 'post'], '/signin', 'App\Http\Controllers\FrontController@signIn');
