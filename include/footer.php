82455effb02fc89f16936241b2ce7961989d29e1
surajit bera <surajit.b@keylines.net>
2025-05-29 12:46:56 +0530

    popup design

--- include/footer.php
+++ include/footer.php
@@ -17,6 +17,8 @@
         </div>
     </div>
 </footer>
+{{-- stickt button --}}
+<a href="<?=url('donation')?>" target="_blank" class="donate-button">Donate to EaRTh</a>
 <!-- js link -->
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
